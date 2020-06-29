099d99bdd83fac7506f180ab776012219c565eb7
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2020-06-28 17:53:10 -0700

    Heroku deployment.

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True #  (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['washiricandjangoapp.herokuapp.com']
+ALLOWED_HOSTS = ['washiricanblog.herokuapp.com']
 
 
 # Application definition
